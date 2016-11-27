e1a3b70fc25233ca976cc7e51d5802c7f3b41d0e
Szymsi <sienkiewicz.szymon@gmail.com>
2016-11-27 16:39:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var a = 5;
 var b = 9;
-var value = (a*a) + (2 * a * b) - (b*b);
+var value = (a * a) + (2 * a * b) - (b * b);
 
-if(+value>0) {
-	console.log("Wynik jest dodatni");
+if(value > 0) {
+	 console.log("Wynik jest dodatni");
 }
+
+else if (value === 0) {
+	 console.log("Wynik równa się 0")
+}
+
 else {
 	console.log("Wynik jest ujemny");
-}
-else if (+value === 0) {
-	console.log("Wynik równa się 0")
 }
